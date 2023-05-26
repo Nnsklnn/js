cd7e2aed1293b469043d3b6f047d6218fe39e698
Anna Akulinina <Anna@Akulinina>
2023-05-26 20:48:02 +0300

    Next save

--- lesson1.js
+++ lesson1.js
@@ -1,4 +1,18 @@
-const myName = 'Anna'
 
-myName = ''
-console.log(myName)
+const name = 'Anna'
+const postsQty = 17
+
+const userProfile = {
+	name,
+	postsQty,
+	hasSignedAgreement: false
+}
+console.log(userProfile)
+
+const myCity = {
+   city: 'New Yourk',
+   cityGreeting() {
+     console.log('Greetings!!!')
+   }
+ }
+ myCity.cityGreeting() // Greetings!!!
